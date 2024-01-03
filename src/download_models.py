8a08886756331ce13c4298e2ddb76c8942510c6d
shirounanashi <154093422+shirounanashi@users.noreply.github.com>
2024-01-03 14:01:46 -0300

    Update download_models.py

--- src/download_models.py
+++ src/download_models.py
@@ -3,10 +3,12 @@ import requests
 
 MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
+VR_DOWNLOAD_LINK = 'https://github.com/shirounanashi/modelos_de_separacao/releases/download/karokee_model'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
 rvc_models_dir = BASE_DIR / 'rvc_models'
+vr_models_dir = BASE_DIR / 'vr_models'
 
 
 def dl_model(link, model_name, dir_name):
@@ -18,10 +20,15 @@ def dl_model(link, model_name, dir_name):
 
 
 if __name__ == '__main__':
-    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
+    mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx', ' MDX23C-8KFFT-InstVoc_HQ.ckpt']
     for model in mdx_model_names:
         print(f'Downloading {model}...')
         dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+        
+    vr_model_names = ['karokee_4band_v2_sn.pth']
+    for model in vr_model_names:
+        print(f'Downloading {model}...')
+        dl_model(VR_DOWNLOAD_LINK, model, vr_models_dir)
 
     rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
     for model in rvc_model_names:
